1aab7e9278fa6508eac92ebeb30e1555efc3bbbb
PALATTTT <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2023-05-07 21:47:17 +0700

    Create cmd.bat

---
+++ cmd.bat
@@ -0,0 +1,25 @@
+echo @off
+echo                URL : 9A3THSP.CF/CMD
+echo                 
+echo                HTTP/3         
+echo                
+echo                DOCUMENT 
+cls            
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+echo             9A3 THỰC HÀNH SƯ PHẠM
+echo            THE WEBSITE OF 9A3 CLASS
+            
+        
